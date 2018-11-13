639b847cb95bbf12de1a6f6612aa4d80c5b36d90
idrisjrnxgbox <idris.xrplsmkrus@gmail.com>
2018-11-13 16:16:22 +0700

    ....

--- app/src/main/java/com/example/rplrus021/midsemester12rpl/adapter.java
+++ app/src/main/java/com/example/rplrus021/midsemester12rpl/adapter.java
@@ -2,6 +2,7 @@ package com.example.rplrus021.midsemester12rpl;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -40,33 +41,38 @@ public class adapter extends RecyclerView.Adapter<adapter.Holder> {
                 .load(only_url.url+data2.getPosterPath())
                 .into(holder.imageView);
         holder.textView.setText(data2.getTitle());
-        holder.button_description.setOnClickListener(new View.OnClickListener() {
+        holder.textView2.setText(data2.getOverview());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final int position1 = position;
                 final String judul = data2.getTitle();
                 final String description = data2.getOverview();
-                final String gambar = data2.getPosterPath();
+                final String gambar = only_url.url+data2.getPosterPath();
                 Intent intent = new Intent(context.getApplicationContext(), description_movie.class);
                 intent.putExtra("judul", judul);
                 intent.putExtra("description", description);
                 intent.putExtra("gambar", gambar);
                 intent.putExtra("position", position1);
                 context.startActivity(intent);
-
             }
         });
-        holder.button_delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final int position2 = position;
-                final String judul = data2.getTitle();
-                database_helper.delete(judul);
-                data.remove(position);
-                notifyItemRemoved(position);
-                notifyItemRangeChanged(position,data.size());
-                    }
-        });
+//        holder.button_description.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                final int position1 = position;
+//                final String judul = data2.getTitle();
+//                final String description = data2.getOverview();
+//                final String gambar = only_url.url+data2.getPosterPath();
+//                Intent intent = new Intent(context.getApplicationContext(), description_movie.class);
+//                intent.putExtra("judul", judul);
+//                intent.putExtra("description", description);
+//                intent.putExtra("gambar", gambar);
+//                intent.putExtra("position", position1);
+//                context.startActivity(intent);
+//
+//            }
+//        });
     }
 
     @Override
@@ -78,16 +84,13 @@ public class adapter extends RecyclerView.Adapter<adapter.Holder> {
         ImageView imageView;
         TextView textView;
         TextView textView2;
-        Button button_description;
-        Button button_delete;
-
+        CardView cardView;
         public Holder(View itemView) {
             super(itemView);
+            cardView = (CardView)itemView.findViewById(R.id.card_view);
             imageView = (ImageView) itemView.findViewById(R.id.image_view);
             textView = (TextView) itemView.findViewById(R.id.textview_judul);
-            textView2 = (TextView) itemView.findViewById(R.id.textview_tanggal);
-            button_description = (Button) itemView.findViewById(R.id.btn_detail);
-            button_delete = (Button) itemView.findViewById(R.id.btn_delete);
+            textView2 = (TextView) itemView.findViewById(R.id.textview_description);
         }
     }
 
--- app/src/main/java/com/example/rplrus021/midsemester12rpl/now_playing.java
+++ app/src/main/java/com/example/rplrus021/midsemester12rpl/now_playing.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -11,6 +12,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,6 +28,7 @@ public class now_playing extends Fragment {
     RecyclerView recyclerView;
     //database_helper database;
     View rootView;
+    ShimmerFrameLayout shimmerFrameLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,23 +37,28 @@ public class now_playing extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_now_playing, container, false);
         recyclerView = (RecyclerView) rootView.findViewById(R.id.recycle_view);
         //database = new database_helper(getActivity());
+        shimmerFrameLayout = (ShimmerFrameLayout) rootView.findViewById(R.id.shimmer_view_container);
+        shimmerFrameLayout.startShimmerAnimation();
         load_data_from_json();
         return rootView;
     }
 
     private void load_data_from_json() {
-        json_api service = retrofitclientinstance.getRetrofitInstance().create(json_api.class);
+        final json_api service = retrofitclientinstance.getRetrofitInstance().create(json_api.class);
         Call<jsonRespond> call = service.getJsonNowPlaying();
         call.enqueue(new Callback<jsonRespond>() {
             @Override
             public void onResponse(Call<jsonRespond> call, Response<jsonRespond> response) {
                 jsonRespond jsonRespond = response.body();
                 data = new ArrayList<>(Arrays.asList(jsonRespond.getResults()));
-                Log.d("responku", "onResponse: "+jsonRespond.getResults());
+                Log.d("responku", "onResponse: " + jsonRespond.getResults());
                 adapter adapter = new adapter(rootView.getContext(), data);
                 LinearLayoutManager linearLayoutManager = new LinearLayoutManager(rootView.getContext());
                 recyclerView.setLayoutManager(linearLayoutManager);
+                recyclerView.setItemAnimator(new DefaultItemAnimator());
                 recyclerView.setAdapter(adapter);
+                shimmerFrameLayout.stopShimmerAnimation();
+                shimmerFrameLayout.setVisibility(View.GONE);
             }
 
             @Override
